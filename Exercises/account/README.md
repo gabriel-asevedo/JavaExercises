004a7ba57b259b77e041734c94c10425d841cd17
Gabriel Asevedo <gabrielamorim002@gmail.com>
2022-04-09 22:22:00 -0300

    new exercise

---
+++ Exercises/account/README.md
@@ -0,0 +1,9 @@
+# English
+Basic exercise for Upcasting, Downcasting and Inheritance compression.
+
+Suppose a bank business has a common account and a business account, where the business account has all common account members, plus a lending limit and a borrowing operation.
+
+# Português
+Exercício básico para compressão de Upcasting, Downcasting e herança.
+
+Suponha que um negócio de banco que possui uma conta comum e uma conta para empresas, sendo que a conta para empresas possui todos membros da conta comum, mais um limite de empréstimo e uma operação de realizar empréstimo.
---
+++ Exercises/account/src/entities/Account.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	protected Double balance;
+
+	public Account() {
+		super();
+	}
+
+	public Account(Integer number, String holder, Double balance) {
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+	}
+
+	public void withdraw(Double amount) {
+		balance -= amount;
+	}
+
+	public void deposit(Double amount) {
+		balance += amount;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+}
